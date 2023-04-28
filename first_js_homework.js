ab7b4fcf97a317a9545ef65c5f3c92b845a51c7c
Harpreet Singh <Harp@Harp-MacPro.home>
2023-04-27 21:31:20 -0400

    first commit

---
+++ first_js_homework.js
@@ -0,0 +1,8 @@
+/* 1. The acronym DRY stands for 'don't repeat yourself'. Software developers should pay attention to DRY because this aims at reducing
+ the repetition of patterns and code duplication, while also avoiding redundancies. */
+/* Differences between const, let, and var: 
+    const - When you never want a variable to change.
+    var - Available throughout the function in which they're declared.
+    let - Variables that are only available inside the block where they're defined. 
+*/
+
